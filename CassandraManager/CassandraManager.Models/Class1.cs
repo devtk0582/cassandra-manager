1b7dedc11881636c13cb6b4906892862e620d685
Ke Tang <jiushizhutk@gmail.com>
2019-03-21 20:49:27 -0400

    initial commit

---
+++ CassandraManager/CassandraManager.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CassandraManager.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ CassandraManager/CassandraManager.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CassandraManager.Services
+{
+    public class Class1
+    {
+    }
+}
